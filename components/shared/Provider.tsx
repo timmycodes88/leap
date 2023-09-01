281845e424ad77030d3203ce98e908f2141e2e58
Tim <tim@brightsidedeveloper.com>
2023-08-31 23:48:42 -0500

    Good Enough

--- components/shared/Provider.tsx
+++ components/shared/Provider.tsx
@@ -1,15 +1,77 @@
 'use client'
 
 import { updateButton } from '@/lib/actions/leap.actions'
-import { useEffect } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { use, useEffect, useRef } from 'react'
 import { Toaster } from 'react-hot-toast'
 
 export default function Provider() {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  //Update Buttons for All Users
   useEffect(() => {
     const i = setInterval(() => updateButton(), 1000 * 60)
     updateButton()
     return () => clearInterval(i)
   }, [])
+
+  //Swipe Navigation
+
+  useEffect(() => {
+    let touchStartX = 0
+    let touchEndX = 0
+    let touchStartY = 0
+    let touchEndY = 0
+    const touchStart = (e: any) => {
+      touchStartX = e.changedTouches[0].screenX
+      touchStartY = e.changedTouches[0].screenY
+    }
+    const touchEnd = (e: any) => {
+      touchEndX = e.changedTouches[0].screenX
+      touchEndY = e.changedTouches[0].screenY
+
+      if (
+        Math.abs(touchEndX - touchStartX) < 100 ||
+        Math.abs(touchEndY - touchStartY) > 50
+      )
+        return
+
+      //Swipe Left
+      if (touchEndX < touchStartX) {
+        switch (pathname) {
+          case '/':
+            return router.push('/team')
+          case '/team':
+            return router.push('/profile')
+          case '/profile':
+          default:
+            return
+        }
+      }
+
+      //Swipe Right
+      if (touchEndX > touchStartX) {
+        switch (pathname) {
+          case '/profile':
+            return router.push('/team')
+          case '/team':
+            return router.push('/')
+          case '/':
+          default:
+            return
+        }
+      }
+    }
+
+    document.addEventListener('touchstart', touchStart)
+    document.addEventListener('touchend', touchEnd)
+
+    return () => {
+      document.removeEventListener('touchstart', touchStart)
+      document.removeEventListener('touchend', touchEnd)
+    }
+  }, [pathname])
 
   return <Toaster />
 }
