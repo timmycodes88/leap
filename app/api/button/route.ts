cac9f7d3ead786c3a8cc737548bd2709ca96e4c3
Tim <tim@brightsidedeveloper.com>
2023-09-01 00:33:32 -0500

    Remove Route

--- app/api/button/route.ts
+++ app/api/button/route.ts
@@ -3,6 +3,7 @@ import { connectToDB } from '@/lib/mongoose'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  return new NextResponse(null, { status: 200 })
   connectToDB()
 
   try {
