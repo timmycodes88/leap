311c0ff7fe78f87e5eba65759c2ae01f6b91fea1
Tim <timvan0118@gmail.com>
2023-09-05 08:09:43 -0500

    Testing

--- app/(root)/(dashboard)/page.tsx
+++ app/(root)/(dashboard)/page.tsx
@@ -6,6 +6,8 @@ import { getUser } from '@/lib/actions/user.actions'
 import { redirect } from 'next/navigation'
 import { getTeams } from '@/lib/actions/team.actions'
 import Refresher from '@/components/shared/Refresher'
+import Card from '@/components/layout/Card'
+import Test from './Test'
 
 export default async function page() {
   const user = await getUser()
@@ -36,6 +38,10 @@ export default async function page() {
         teams={teams.sort(t => (t.teamId === user.teamId ? -1 : 1))}
         userId={user.id}
       />
+      <Card>
+        <p>Text me @ 913-636-3773 if your time is not within a couple minutes of this time: </p>
+<Test />
+      </Card>
     </div>
   )
 }
