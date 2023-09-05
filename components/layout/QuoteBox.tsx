b462f58cc398c75bff5996ef49f9af239367d51d
Tim <tim@brightsidedeveloper.com>
2023-09-05 06:43:55 -0500

    Last One

--- components/layout/QuoteBox.tsx
+++ components/layout/QuoteBox.tsx
@@ -7,7 +7,7 @@ export default function QuoteBox({ quote, author }: QuoteBoxProps) {
   return (
     <div className='flex flex-col overflow-x-hidden bg-gray-800 p-4 justify-around items-center rounded-3xl'>
       <span className='text-sm font-semibold text-center'>{quote}</span>
-      <span className='italic text-sm font-light'>{author}</span>
+      <span className='italic text-sm font-light'>-{author}</span>
     </div>
   )
 }
