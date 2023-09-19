4b3b4978b64c75c61fec3f7b9e22ea2f0424fb30
Tim <tim@brightsidedeveloper.com>
2023-09-18 20:55:30 -0500

    Tooltip

--- components/shared/ActionTooltip.tsx
+++ components/shared/ActionTooltip.tsx
@@ -1,33 +1,76 @@
 'use client'
 
+import { usePathname } from 'next/navigation'
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip'
-
-interface ActionTooltipProps {
-  label: string
-  children: React.ReactNode
-  side?: 'top' | 'bottom' | 'left' | 'right'
-  align?: 'start' | 'center' | 'end'
-}
+  MutableRefObject,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
+import { createPortal } from 'react-dom'
 
 export default function ActionTooltip({
   label,
   children,
-  side,
-  align,
-}: ActionTooltipProps) {
+}: {
+  label: string
+  children: React.ReactNode
+}) {
+  const isLeft = usePathname() === '/'
+  const [open, setOpen] = useState(false)
+  const onClick = () => setOpen(prev => !prev)
+  const tooltipRef = useRef<HTMLSpanElement>(null)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  useEffect(() => {
+    const tooltip = tooltipRef.current
+    const button = buttonRef.current
+
+    if (open && tooltip && button) {
+      const { top, left, width } = button.getBoundingClientRect()
+
+      tooltip.style.top = `${top - 40}px`
+      tooltip.style.left = `${
+        isLeft ? left - 70 : left - (tooltip.offsetWidth - width) / 2
+      }px`
+    }
+
+    const handleClickOutside = (e: MouseEvent) => {
+      if (tooltip && !tooltip.contains(e.target as Node)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside)
+    return () => document.removeEventListener('click', handleClickOutside)
+  }, [open])
   return (
-    <TooltipProvider>
-      <Tooltip delayDuration={50}>
-        <TooltipTrigger asChild>{children}</TooltipTrigger>
-        <TooltipContent side={side} align={align}>
-          <p className='font-semibold text-sm'>{label}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <>
+      {open &&
+        createPortal(
+          <Tooltip tooltipRef={tooltipRef} content={label} />,
+          document.body
+        )}
+      <>
+        <button ref={buttonRef} onClick={onClick}>
+          {children}
+        </button>
+      </>
+    </>
   )
 }
+
+const Tooltip = ({
+  content,
+  tooltipRef,
+}: {
+  content: string
+  tooltipRef: React.RefObject<HTMLSpanElement>
+}) => (
+  <span
+    ref={tooltipRef}
+    className='fixed top-0 left-0 z-[1000] bg-gray-900 border border-gray-600 text-white px-2 py-1 rounded-md shadow-md'
+  >
+    {content}
+  </span>
+)
